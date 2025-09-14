05e5ed4d6770f45c0a5668886742d0d4b19997ab
Jonathan MUGISHA <j.mugisha3@alustudent.com>
2025-09-14 22:53:51 +0200

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 """
-Return the fewest operations needed to reach 
+Return the fewest operations needed to reach
 n 'H' characters using Copy All and Paste.
 
 - def minOperations(n): → int
 - Returns 0 if n ≤ 1
 - Uses prime factorization to compute minimal steps
 """
+
 
 def minOperations(n):
     if n <= 1:
@@ -23,4 +24,3 @@ def minOperations(n):
         factor += 1
 
     return operations
-    
